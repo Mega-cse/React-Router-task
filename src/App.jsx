13f7caa86eedbb9ea3b69997cfa80aada4a27cd2
Megavardhini.P <megavardhini1958@gmail.com>
2024-07-06 12:35:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,18 +14,9 @@ import './App.css'
 
 function App() {
   const products = [
-{
-      id: 1,
-      name: 'Hot Topics That You Need To Know In Full Stack Developer Syllabus',
-      author: 'Isha Sharma',
-      image: 'https://www.guvi.in/blog/wp-content/uploads/2021/09/Full-Stack-Developer-course-syllabus.png',
-      date   :"03 Jul,2024",
-      readtime:"4 Min Read",
-      course:"fsd",
 
-    },
     {
-      id: 2,
+      id: 1,
       name: 'How Long Would It Take to Be a Full Stack Developer?',
       author: 'Meghana D',
       image: 'https://www.guvi.in/blog/wp-content/uploads/2023/07/how-long-it-would-it-take-to-learn-full-stack-development_-1536x804.webp',
@@ -36,7 +27,7 @@ function App() {
     },
    
     {
-      id: 3,
+      id: 2,
       name: ' Top 10 Product-Based Companies for Full-Stack Developers [2024]',
       author: 'Tushar Vinocha',
       image: 'https://www.guvi.in/blog/wp-content/uploads/2023/10/Featured-Image-top-product-based-companies-for-full-stack-developers.webp',
@@ -48,7 +39,7 @@ function App() {
     },
     
     {
-      id: 4,
+      id: 3,
       name: '7 Best Full-Stack Development Online Courses',
       author: 'Isha Sharma',
       image: 'https://www.guvi.in/blog/wp-content/uploads/2023/08/Feature-image-Best-Full-Stack-Development-Online-Courses.webp',
@@ -60,7 +51,7 @@ function App() {
     },
     
     {
-      id: 5,
+      id: 4,
       name: 'Top 10 High Paying Non-Coding Jobs in Data Science in 2024',
       author: 'Isha Sharma',
       image: 'https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-Top-High-Paying-Non-Coding-Jobs-in-Data-Science.webp',
@@ -72,7 +63,7 @@ function App() {
     },  
     
     {
-      id: 6,
+      id: 5,
       name: 'Top Product-Based Companies for Data Science Freshers',
       author: 'Saakshi Priyadarshini',
       image: 'https://www.guvi.in/blog/wp-content/uploads/2023/11/feature-image-product-based-companies-for-data-science-freshers.webp',
@@ -84,7 +75,7 @@ function App() {
     },
     
     {
-      id: 7,
+      id: 6,
       name: 'Non-Coding Jobs in Cybersecurity: A Comprehensive Guide',
       author: 'Jaishree Tomar',
       image: 'https://www.guvi.in/blog/wp-content/uploads/2023/12/Feature-image-Non-Coding-Jobs-in-Cybersecurity.webp',
@@ -94,7 +85,7 @@ function App() {
     },
     
     {
-      id: 8,
+      id: 7,
       name: 'What Is Hacking? Types of Hacking & More',
       author: ' Meghana D',
       image: 'https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-What-Is-Hacking_-Types-of-Hacking-More.webp',
@@ -104,7 +95,7 @@ function App() {
 
     },
     {
-      id:9,
+      id:8,
       name:'FullStackDeveloper',
       author:'Tushar Vinocha',
       image:'https://media.swipepages.com/2022/12/5fcde7acf64f9100108c719e/fsd-300.webp',
@@ -114,7 +105,7 @@ function App() {
 
     },
     {
-      id:10,
+      id:9,
       name:'UI/UX Developer',
       author:'Tushar Vinocha',
       image:'https://media.swipepages.com/2022/12/5fcde7acf64f9100108c719e/ux_ui-design-300.webp',
